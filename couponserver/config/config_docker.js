6bb2e14ea7071cb5a18282d0a078651457c8f240
Manuel Beuttler <manuel.beuttler@de.trumpf.com>
2017-10-18 15:32:10 +0200

    updated dockerfile and config for couponserver

--- couponserver/config/config_docker.js
+++ couponserver/config/config_docker.js
@@ -4,7 +4,7 @@ const self = {};
 
 self.DATABASE_NAME = '/usr/src/db/coupon.db';
 self.LOG_LEVEL = 'debug';
-self.CERT_PATH = '/usr/src/app/cert/';
+self.CERT_PATH = '/usr/src/cert/';
 
 
 module.exports = self;
